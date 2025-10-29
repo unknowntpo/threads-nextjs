f66c54976aebe99374d3534cd22cdf26ac795a9a
Eric Chang <e850506@gmail.com>
2025-10-29 18:44:31 +0800

    chore(terraform): remove unused cloudrun module

--- terraform/modules/cloudrun/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "nextjs_url" {
-  description = "Next.js Cloud Run URL"
-  value       = google_cloud_run_v2_service.nextjs.uri
-}
-
-output "nextjs_service_name" {
-  description = "Next.js service name"
-  value       = google_cloud_run_v2_service.nextjs.name
-}
